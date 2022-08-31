db394a63b5f787f4a34d281dc93db5a4668a7b33
Tesmi <tusharmaurya2001@gmail.com>
2022-08-31 22:17:35 +0530

    ci: adding under-construction txt on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,11 @@ export default function HomePage() {
       <main>
         <section>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
+            <h1 className='font-medium text-sky-50'>
+              Website Under Construction...
+            </h1>
+            <br />
+            <h1>⚠️</h1>
             <footer className='absolute bottom-2 text-gray-700'>
               © {new Date().getFullYear()} By{' '}
               {/* // Todo: Insert Page link below */}
