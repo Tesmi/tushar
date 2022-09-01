f433f1159a47fdce220c5758bdd2321bb7659e4d
Tesmi <tusharmaurya2001@gmail.com>
2022-09-01 19:52:08 +0530

    ci: refactoring footer code

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react';
+import UnderlineLink from '@/components/links/UnderlineLink';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <div className='flex items-center justify-center'>
+        <footer className='absolute bottom-2 text-gray-700 '>
+          © {new Date().getFullYear()} By{' '}
+          {/* // Todo: Insert Page link below */}
+          <UnderlineLink href=''>Tesmi</UnderlineLink>
+        </footer>
+      </div>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import UnderlineLink from '@/components/links/UnderlineLink';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
   return (
     <Layout>
       <Seo templateTitle='Tushar' description='Portfolio Landking Page' />
-
       <main>
         <section>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
@@ -17,11 +15,6 @@ export default function HomePage() {
             </h1>
             <br />
             <h1>⚠️</h1>
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              {/* // Todo: Insert Page link below */}
-              <UnderlineLink href=''>Tushar</UnderlineLink>
-            </footer>
           </div>
         </section>
       </main>
